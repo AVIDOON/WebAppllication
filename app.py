f7a910ccb2e22764b4d524b3a2977c924adb6cda
AVIDOON <44518438+AVIDOON@users.noreply.github.com>
2024-07-03 00:12:51 +0530

    Adding css and making new js files

--- app.py
+++ app.py
@@ -1,55 +1,56 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 from datetime import date
-date_format = "%d/%m/%Y"
+date_format1 = "%d/%m/%Y"
+date_format2 = '%Y-%m-%d'
 
 monthlyROI = 5
 annualROI = 12 * monthlyROI
-includetoday = False
+todayIncluded = 'No'
 
 app = Flask(__name__)
 
 
 def date_diff_in_days(start_date, end_date):
-    start_date = datetime.strptime(start_date, '%Y-%m-%d')
-    end_date = datetime.strptime(end_date, '%Y-%m-%d')
     return abs((end_date - start_date).days)
 
-def calculateInterest(principalAmount, startDate, endDate):
-    # startDate = datetime.strptime(input('Enter Starting Date : '), date_format)
-    # isEndDateToday = input('Is End Date today? True/False: ')
 
-    # if isEndDateToday == True:
-    #     endDate = datetime.today()
-    # else:
-    #     endDate = datetime.strptime(input('Enter End Date : '), date_format)
-    datediff = date_diff_in_days(startDate, endDate)
-    if includetoday:
-        datediff += 1
+@app.route('/calculate', methods=['GET', 'POST'])
+def calculateInterest():
+    if request.method == 'POST':
+        principalAmount = float(request.form['principal'])
+        startDate = request.form['start_date']
+        endDate = request.form['end_date']
+        includetoday = request.form.get('includeToday')
 
-    calculatedInterest = (principalAmount * annualROI * datediff) / (36500)
-    totalAmount = principalAmount + calculatedInterest
+        start_Date =  datetime.strptime(startDate, date_format2).date()
+        end_Date =  datetime.strptime(endDate, date_format2).date()
 
-    # print('Principal Amount : ', principalAmount)
-    # print('Start Date : ', startDate)
-    # print('End Date : ', endDate)
-    # print(datediff, "days")
-    # print('Interest : ', int(calculatedInterest))
-    # print('Total Amount : ', int(totalAmount))
-    return calculatedInterest
+        datediff = date_diff_in_days(start_Date, end_Date)
+        if includetoday:
+            datediff += 1
+            todayIncluded = 'Yes'
+
+        calculatedInterest = round((principalAmount * annualROI * datediff) / (36500),2)
+        totalAmount = round((principalAmount + calculatedInterest), 2)
+
+        calculation_details = {
+            'principal': principalAmount,
+            'start_date': start_Date,
+            'end_date': end_Date,
+            'simple_interest' : calculatedInterest,
+            'amount': totalAmount,
+            'today_included' : todayIncluded
+        }
+
+        return render_template('details.html', details=calculation_details)
+    else:
+        return redirect(url_for('index'))
+
 
 # Define a route to render the form
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-
-        principalAmount = float(request.form['principal'])
-        startDate = request.form['start_date']
-        endDate = request.form['end_date']
-        simpleInterest = calculateInterest(principalAmount, startDate, endDate)
-
-        return jsonify({'amount': simpleInterest})
-
     # Render the form template if it's a GET request
     return render_template('form.html')
 
---
+++ static/javascript/app.js
@@ -0,0 +1,26 @@
+function setToday(fieldId) {
+    var today = new Date().toISOString().split('T')[0];
+    document.getElementById(fieldId).value = today;
+}
+
+    // Make AJAX request to Flask backend
+    $(document).ready(function() {
+        $('#calculationForm').on('submit', function(event) {
+            event.preventDefault(); // Prevent the form from submitting normally
+
+            $.ajax({
+                type: 'POST',
+                url: '/calculate',
+                data: $(this).serialize(),
+                success: function(response) {
+                    // Redirect to details page or display details inline
+                    window.location.href = '/details'; // Redirect to details page
+                    // Alternatively, display details inline
+                    // $('#amountContainer').text('Calculated Amount: ' + response.amount);
+                },
+                error: function(error) {
+                    console.log('Error:', error);
+                }
+            });
+        });
+    });
